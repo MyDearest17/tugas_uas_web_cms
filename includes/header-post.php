af57879cd80718c4f4f3b9b17b93721aac98c1c3
Rizki Zulfihadi <rizkizulfihadi17@gmail.com>
2024-01-24 20:16:57 +0700

    first init in github

---
+++ includes/header-post.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title><?= $title ?></title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="assets/modules/bootstrap-social/bootstrap-social.css">
+
+<!-- CSS Libraries -->
+<link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css">
+<link rel="stylesheet" href="assets/modules/jquery-selectric/selectric.css">
+<link rel="stylesheet" href="assets/modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css">
+
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/styleAdmin.css">
+  <link rel="stylesheet" href="assets/css/components.css">
+  <script src="assets/modules/sweetalert/sweetalert.min.js"></script>
+<!-- Start GA -->
+
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
